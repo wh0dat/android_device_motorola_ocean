9f72df745ec132db47a74f77cf2e67dcf7849c8c
Barry Allen <flash@remainsilent.net>
2019-09-16 14:08:26 -0500

    ocean: AEX Bringup

--- aosp_ocean.mk
+++ aosp_ocean.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from ocean device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -58,10 +58,12 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     libgptutils \
     libz
 
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := ocean
 PRODUCT_MANUFACTURER := motorola
-PRODUCT_NAME := lineage_ocean
+PRODUCT_NAME := aosp_ocean
 PRODUCT_MODEL := moto g(7) power
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
