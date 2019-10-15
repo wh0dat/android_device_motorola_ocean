de002b183e2950999f26864c286f699a8af264f9
Barry Allen <whodat@remainsilent.net>
2019-10-15 05:48:21 +0000

    ocean: wifi: disable mac randomization

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,6 +163,7 @@ WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_AP  := "ap"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
+WIFI_HIDL_FEATURE_DISABLE_AP_MAC_RANDOMIZATION := true
 
 # Inherit from the proprietary version
 -include vendor/motorola/ocean/BoardConfigVendor.mk
